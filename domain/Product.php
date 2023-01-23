a915a298b1f2fa34ffee972c8d1c8366d1f4b706
AssoDePicche <samuelprado730@gmail.com>
2023-01-23 19:35:52 -0300

    Feat: Add Product class

---
+++ domain/Product.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Cart;
+
+use Cart\Contract\ProductInterface;
+use InvalidArgumentException;
+
+final class Product implements ProductInterface
+{
+    public function __construct(
+        private readonly string $name,
+        private float $unitPrice,
+        private int $availableQuantity
+    ) {
+        strlen($name) === 0 && throw new InvalidArgumentException();
+
+        $unitPrice < 0 && throw new InvalidArgumentException();
+
+        $availableQuantity < 0 && throw new InvalidArgumentException();
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getUnitPrice(): float
+    {
+        return $this->unitPrice;
+    }
+
+    public function getAvailableQuantity(): int
+    {
+        return $this->availableQuantity;
+    }
+
+    public function decreaseAvailableQuantity(int $amount = 1): void
+    {
+        $this->availableQuantity - $amount < 0 && throw new InvalidArgumentException();
+
+        $this->availableQuantity -= $amount;
+    }
+}
