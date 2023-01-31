ad34bb6bffcfebdfc471213506fab684a335c05d
AssoDePicche <samuelprado730@gmail.com>
2023-01-31 17:28:50 -0300

    Refactor: Move the increase and decrease methods to change quantity method

--- domain/contract/CartItemInterface.php
+++ domain/contract/CartItemInterface.php
@@ -10,7 +10,5 @@ interface CartItemInterface
 
     public function getSubtotal(): float;
 
-    public function increaseQuantity(int $amount = 1): void;
-
-    public function decreaseQuantity(int $amount = 1): void;
+    public function changeQuantity(int $amount): void;
 }
