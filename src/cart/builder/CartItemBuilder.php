060d3a65d59d0c52ed28970bd309d15b0fcc3ed7
AssoDePicche <samuelprado730@gmail.com>
2023-02-09 21:13:16 -0300

    Feat: Add Cart Item Builder class

---
+++ src/cart/builder/CartItemBuilder.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cart\Builder;
+
+use Cart\CartItem;
+use Cart\Contract\CartItemInterface;
+use Cart\Contract\ProductInterface;
+
+final class CartItemBuilder
+{
+    public static function getCartItem(ProductInterface $item, int $quantity): CartItemInterface
+    {
+        return new CartItem($item, $quantity);
+    }
+}
