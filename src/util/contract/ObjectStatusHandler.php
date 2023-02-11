ef0a1f58f064bf47e13e7c4dda98c15aef1ba7bc
AssoDePicche <samuelprado730@gmail.com>
2023-02-11 17:43:49 -0300

    Feat: Add Object Status Handler interface

---
+++ src/util/contract/ObjectStatusHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Util\Contract;
+
+interface ObjectStatusHandler
+{
+    public function export(object $object): void;
+
+    public function import(object $object): object;
+}
