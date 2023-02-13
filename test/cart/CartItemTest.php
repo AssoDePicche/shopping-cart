1baddf0ab587974c11549605db3dac3d2f7f99c3
AssoDePicche <samuelprado730@gmail.com>
2023-02-13 19:49:14 -0300

    Refactor: Update methods names

--- test/cart/CartItemTest.php
+++ test/cart/CartItemTest.php
@@ -15,36 +15,36 @@ final class CartItemTest extends TestCase
     {
         $this->expectException(InvalidArgumentException::class);
 
-        $product = ProductBuilder::getProduct('Kubidai Hikiukenin', 4990, 12);
+        $product = ProductBuilder::build('Kubidai Hikiukenin', 4990, 12);
 
-        CartItemBuilder::getCartItem($product, 20);
+        CartItemBuilder::build($product, 20);
     }
 
     public function test_a_quantity_equal_to_zero_should_throw_invalid_argument_exception(): void
     {
         $this->expectException(InvalidArgumentException::class);
 
-        $product = ProductBuilder::getProduct('Kubidai Hikiukenin', 4990, 12);
+        $product = ProductBuilder::build('Kubidai Hikiukenin', 4990, 12);
 
-        CartItemBuilder::getCartItem($product, 0);
+        CartItemBuilder::build($product, 0);
     }
 
     public function test_a_quantity_smaller_than_zero_should_throw_invalid_argument_exception(): void
     {
         $this->expectException(InvalidArgumentException::class);
 
-        $product = ProductBuilder::getProduct('Kubidai Hikiukenin', 4990, 12);
+        $product = ProductBuilder::build('Kubidai Hikiukenin', 4990, 12);
 
-        CartItemBuilder::getCartItem($product, -10);
+        CartItemBuilder::build($product, -10);
     }
 
     public function test_increase_an_amount_greater_than_available_quantity_should_throw_invalid_argument_exception(): void
     {
         $this->expectException(InvalidArgumentException::class);
 
-        $product = ProductBuilder::getProduct('Kubidai Hikiukenin', 4990, 12);
+        $product = ProductBuilder::build('Kubidai Hikiukenin', 4990, 12);
 
-        $item = CartItemBuilder::getCartItem($product, 12);
+        $item = CartItemBuilder::build($product, 12);
 
         $item->changeQuantity(1);
     }
@@ -53,9 +53,9 @@ final class CartItemTest extends TestCase
     {
         $this->expectException(InvalidArgumentException::class);
 
-        $product = ProductBuilder::getProduct('Kubidai Hikiukenin', 4990, 12);
+        $product = ProductBuilder::build('Kubidai Hikiukenin', 4990, 12);
 
-        $item = CartItemBuilder::getCartItem($product, 2);
+        $item = CartItemBuilder::build($product, 2);
 
         $item->changeQuantity(-3);
     }
--- test/cart/ProductTest.php
+++ test/cart/ProductTest.php
@@ -14,21 +14,21 @@ final class ProductTest extends TestCase
     {
         $this->expectException(InvalidArgumentException::class);
 
-        ProductBuilder::getProduct('', 4990, 12);
+        ProductBuilder::build('', 4990, 12);
     }
 
     public function test_available_quantity_smaller_than_zero_should_throw_invalid_argument_exception(): void
     {
         $this->expectException(InvalidArgumentException::class);
 
-        ProductBuilder::getProduct('Kubidai Hikiukenin', 4990, -1);
+        ProductBuilder::build('Kubidai Hikiukenin', 4990, -1);
     }
 
     public function test_decrease_an_amount_greater_than_available_quantity_should_throw_invalid_argument_exception(): void
     {
         $this->expectException(InvalidArgumentException::class);
 
-        $product = ProductBuilder::getProduct('Kubidai Hikiukenin', 4990, 2);
+        $product = ProductBuilder::build('Kubidai Hikiukenin', 4990, 2);
 
         $product->changeAvailableQuantity(3);
     }
