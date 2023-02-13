fcb6e3030afbc00bad1bc2f55bf74420f4a8b862
AssoDePicche <samuelprado730@gmail.com>
2023-02-13 18:12:15 -0300

    Feat: Add Model Interface

--- src/database/model/ModelFactory.php
+++ src/database/model/ModelFactory.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Database\Model;
 
 use Database\Database;
+use Database\Model\Contract\ModelInterface;
 
 final class ModelFactory
 {
-    public static function build(): Model
+    public static function build(): ModelInterface
     {
         return new Model(Database::getConnection());
     }
